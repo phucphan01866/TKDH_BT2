b9b3148da2cb453788538acd3d5d56a1dda04f46
PhucPhan <phucphan01866@gmail.com>
2024-03-13 10:30:21 +0700

    Update File_Main.py

--- File_Main.py
+++ File_Main.py
@@ -61,11 +61,6 @@ def draw_2_cham_gach(count0, count1, count2, count3, count0_max, count1_max, cou
 
 #Hàm chính, nhập các giá trị độ dài tại đây (từ tọa độ 2 điểm ban đầu), tính toán vị trí để putpixel
 def draw(x1,y1,x2,y2,choice):
-    
-    #Chấm 2 chấm tại điểm đầu và cuối
-    plt.plot(x1,y1, marker='o', color='r')
-    plt.plot(x2,y2, marker='o',color='b')
-    
     Dx = abs(x2 - x1)
     Dy = abs(y2 - y1)
     
@@ -90,6 +85,7 @@ def draw(x1,y1,x2,y2,choice):
         x_change=-1
     
     x,y=x1,y1
+    
     if (Dx>Dy):
         d  = 2*Dy -   Dx
         D1 = 2*Dy - 2*Dx
@@ -103,6 +99,7 @@ def draw(x1,y1,x2,y2,choice):
             else:
                d += D2
             x+=x_change
+            #----___----
             if choice==1:
                 count0, count1 = draw_net_dut(count0, count1,count0_max, count1_max, X, Y, x, y)
             elif choice==2:
@@ -123,14 +120,47 @@ def draw(x1,y1,x2,y2,choice):
             else:
                 d+= D2
             y+=y_change
-            if choice==1:
+            if choice==0:
+                plt.plot([X,x],[Y,y], linestyle='solid', color='c')
+            elif choice==1:
                 count0, count1 = draw_net_dut(count0, count1,count0_max, count1_max, X, Y, x, y)
             elif choice==2:
                 count0, count1, count2 = draw_1_cham_gach(count0, count1, count2, count0_max, count1_max, count2_max, X, Y, x, y)
             elif choice==3:
                 count0, count1, count2, count3 = draw_2_cham_gach(count0, count1, count2, count3, count0_max, count1_max, count2_max, count3_max, X, Y, x, y)
-                
+    if choice!=0:
+        #Chấm 2 chấm tại điểm đầu và cuối
+        plt.plot(x1,y1, marker='o', color='r')
+        plt.plot(x2,y2, marker='o',color='b')            
+
+#Hàm vẽ HCN
+def draw_rect(x1,y1,x2,y2):
+    if x1 > x2:
+        swap_x, swap_y=x1, y1
+        x1,y1=x2,y2
+        x2,y2=swap_x,swap_y
     
+    x,y=x1,y1
+    print('here2')
+    Dy=y1-y2
+    
+    while x!=x2:
+        plt.plot([x,x],[y,y+Dy], linestyle='solid', color='b')
+        plt.plot([x,x+1],[y,y], linestyle='solid', color='c')
+        plt.plot([x,x+1],[y+Dy,y+Dy], linestyle='solid', color='c')
+        x+=1
+    plt.plot([x1,x1],[y,y+Dy], linestyle='solid', color='c')
+    plt.plot([x2,x2],[y,y+Dy], linestyle='solid', color='c')
+    
+    
+#Hàm trung chuyển giữa vẽ đường thẳng và chữ nhật
+def Input_draw(x1,y1,x2,y2, choice):
+    if choice < 4:
+        draw(x1,y1,x2,y2,choice)
+    elif choice == 4:
+        print('here1')
+        draw_rect(x1,y1,x2,y2)
+           
 def axis(ax, x1, y1, x2, y2):
     line = []
 
@@ -187,7 +217,9 @@ def axis(ax, x1, y1, x2, y2):
     ax.plot(x_values, y_values, color='black', linestyle='-', linewidth=2)
 
 # Thiết lập kích thước của hình vẽ
-fig, ax = plt.subplots(figsize=(10, 10))
+fig, ax = plt.subplots(figsize=(9, 9))
+ax.set_box_aspect(aspect=1)
+
 
 # Vẽ lưới
 ax.grid(True, linestyle='--', alpha=0.7)
@@ -196,20 +228,21 @@ ax.grid(True, linestyle='--', alpha=0.7)
 plt.grid(True)
 plt.title('Hệ tọa độ 2D')
 
-
 def get_user_input():
-    x1, y1 = 50, 10
-    x2, y2 = 50, 70
+    x1, y1 = 10, 50
+    x2, y2 = 100, 200
     return x1, y1, x2, y2
 # Get user input
 user_input = get_user_input()
 axis_x=(max(user_input[0],user_input[2]))
 axis_y=(max(user_input[1],user_input[3]))
+
 axis_x_max, axis_x_min=min(-10,int(-1.3*axis_x)), max(10,int(1.3*axis_x))
 axis_y_max, axis_y_min=min(-10,int(-1.3*axis_y)), max(10,int(1.3*axis_y))
 axis(ax, axis_x_min, 0, axis_x_max, 0)  # Trục X
 axis(ax, 0, axis_y_min, 0, axis_y_max)  # Trục Y
 
-draw(*user_input, 3)
+Input_draw(*user_input, 4)
+    
 plt.show()
 
